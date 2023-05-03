c319757059b3808fc70c6919acf0074408f17d0f
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-05-03 19:24:52 -0300

    update style

--- init.lua
+++ init.lua
@@ -133,10 +133,18 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
-        theme = 'gruvbox-baby',
+        icons_enabled = true,
+        theme = 'gruvbox_dark',
         component_separators = '|',
-        section_separators = '',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 }, },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 }, }
       },
     },
   },
