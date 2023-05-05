17c54334ec9918bf936b45bf9a4a7c793a15812c
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-05-05 15:25:27 -0300

    update shortcuts

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -5,8 +5,9 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
         vim.keymap.set('n', '<Leader>l', ':BufferLineCycleNext<CR>', { silent = true, desc = "Go to the next buffer" })
-        vim.keymap.set('n', '<Leader>h', ':BufferLineCyclePrev<CR>', { silent = true, desc = "Go to the  previous buffer" })
-        vim.keymap.set('n', '<C-w>', ':write|bdelete<CR>', { silent = true, desc = "Save and close current buffer" })
+        vim.keymap.set('n', '<Leader>h', ':BufferLineCyclePrev<CR>',
+            { silent = true, desc = "Go to the  previous buffer" })
+        vim.keymap.set('n', '<C-w>', ':bdelete<CR>', { silent = true, desc = "Save and close current buffer" })
         vim.keymap.set('n', '<C-s>', ':write|:Format<CR>', { silent = true, desc = "Save current buffer" })
         vim.opt.termguicolors = true
         require("bufferline").setup {}
