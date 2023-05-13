b76e447a68716ea03ecd9341ec3422ec281fe68c
phelipeoliveira <phelipewesleydeoliveira@gmail.com>
2023-05-13 14:14:59 -0300

    edit close buffer shortcut

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -7,7 +7,7 @@ return {
         vim.keymap.set('n', '<Leader>l', ':BufferLineCycleNext<CR>', { silent = true, desc = "Go to the next buffer" })
         vim.keymap.set('n', '<Leader>h', ':BufferLineCyclePrev<CR>',
             { silent = true, desc = "Go to the  previous buffer" })
-        vim.keymap.set('n', '<C-w>', ':bd<CR>', { silent = true, desc = "Close current buffer" })
+        vim.keymap.set('n', '<C-q>', ':bd<CR>', { silent = true, desc = "Close current buffer" })
         vim.keymap.set('n', '<C-s>', ':w<CR>', { silent = true, desc = "Save current buffer" })
         vim.opt.termguicolors = true
         require("bufferline").setup {}
