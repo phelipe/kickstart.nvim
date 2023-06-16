ceda4fad3cba7f43535bef6401b89c95d523f698
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-06-15 22:19:34 -0300

    feat: add snipetts package

---
+++ lua/custom/plugins/luasnip.lua
@@ -0,0 +1,9 @@
+-- package for work with snippets like in vscode
+return {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require 'luasnip'.filetype_extend("dart", { "flutter" })
+    end
+}
