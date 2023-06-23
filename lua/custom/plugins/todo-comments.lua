2b16b4c5d9d84d4ac59446c02ab1aec6af676b1a
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-06-23 17:39:09 -0300

    feat: add new plugins

---
+++ lua/custom/plugins/todo-comments.lua
@@ -0,0 +1,9 @@
+-- Add plugin to esay search TODO comments
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("todo-comments").setup({})
+        vim.keymap.set('n', '<Leader>st', ':TodoTelescope<CR>', { silent = true, desc = "[S]earch [T]odos" })
+    end
+}
