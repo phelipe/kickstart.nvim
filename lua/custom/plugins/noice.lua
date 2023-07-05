9de5c239dd5e7c3400bf9adcfe5a7fec9a41757f
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-07-05 12:31:47 -0300

    feat: disable noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,6 +1,7 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
+    enabled = false,
     opts = {
         -- add any options here
     },
@@ -10,7 +11,7 @@ return {
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
+        -- "rcarriga/nvim-notify",
     },
     config = function()
         require("telescope").load_extension("noice")
