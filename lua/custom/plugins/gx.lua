95c4caedbf78a8542f769193a4d665256e8cdaa8
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-07-26 16:37:24 -0300

    feat: add gx plugin and update config of debug

---
+++ lua/custom/plugins/gx.lua
@@ -0,0 +1,7 @@
+-- plugin for open browser links with gx
+return {
+    "chrishrb/gx.nvim",
+    event = { "BufEnter" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true, -- default settings
+}
