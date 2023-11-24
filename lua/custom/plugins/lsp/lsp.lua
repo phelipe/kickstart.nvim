7a9a95316484973dedd461325f12b9040646efcc
phelipeoliveira <phelipewesleydeoliveira@gmail.com>
2023-11-24 11:15:14 -0300

    GD now in middle of the screen

--- lua/custom/plugins/lsp/lsp.lua
+++ lua/custom/plugins/lsp/lsp.lua
@@ -35,7 +35,7 @@ return {
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+      nmap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>zz', '[G]oto [D]efinition')
       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
