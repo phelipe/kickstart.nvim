896464f64c5b44638c901febf6daa440c013291d
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-11-30 14:29:47 -0300

    add live-grep-args extension

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -13,6 +13,9 @@ return {
       end,
     },
     'jonarrien/telescope-cmdline.nvim',
+    {
+      'nvim-telescope/telescope-live-grep-args.nvim',
+    },
   },
   config = function()
     require('telescope').setup {
@@ -27,6 +30,7 @@ return {
     }
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'cmdline')
+    pcall(require('telescope').load_extension 'live_grep_args')
     -- See `:help telescope.builtin`
     vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
@@ -42,7 +46,8 @@ return {
     vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+    -- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sg', require('telescope').extensions.live_grep_args.live_grep_args, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>:', '<cmd>Telescope cmdline<cr>', { desc = 'Cmd Line' })
   end,
