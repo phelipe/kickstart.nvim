1ae757f70e0ee56cad5d195d2d0a80310dbc9956
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2023-12-01 22:13:33 -0300

    update to harpoon 2

--- lua/custom/plugins/haarpoon.lua
+++ lua/custom/plugins/haarpoon.lua
@@ -1,15 +1,25 @@
 -- plugin for fast movimentation between files in nvim
 return {
   'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
   event = { 'BufEnter' },
   dependencies = 'nvim-lua/plenary.nvim',
   config = function()
-    -- if you wnat use it with telescope
-    -- vim.keymap.set('n', '<leader>sm', ":Telescope harpoon marks<CR>", { desc = "Harpoon [S]how [M]arks" })
-    vim.keymap.set('n', '<leader>n', ":lua require('harpoon.mark').rm_file()<CR>", { desc = 'Harpoon remove [M]ark' })
-    vim.keymap.set('n', '<leader>m', ":lua require('harpoon.mark').add_file()<CR>", { desc = 'Harpoon add [M]ark' })
-    vim.keymap.set('n', '<leader>sm', ":lua require('harpoon.ui').toggle_quick_menu()<CR>", { desc = 'Harpoon [S]how [M]arks' })
-    vim.keymap.set('n', '<leader>j', ":lua require('harpoon.ui').nav_next()<CR>", { desc = 'Harpoon Next Mark' })
-    vim.keymap.set('n', '<leader>k', ":lua require('harpoon.ui').nav_prev()<CR>", { desc = 'Harpoon Previous Mark' })
+    local harpoon = require 'harpoon'
+    vim.keymap.set('n', '<leader>n', function()
+      harpoon:list():remove()
+    end, { desc = 'Harpoon remove [M]ark' })
+    vim.keymap.set('n', '<leader>m', function()
+      harpoon:list():append()
+    end, { desc = 'Harpoon add [M]ark' })
+    vim.keymap.set('n', '<leader>sm', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon [S]how [M]arks' })
+    vim.keymap.set('n', '<leader>j', function()
+      harpoon:list():next()
+    end, { desc = 'Harpoon Next Mark' })
+    vim.keymap.set('n', '<leader>k', function()
+      harpoon:list():prev()
+    end, { desc = 'Harpoon Previous Mark' })
   end,
 }
