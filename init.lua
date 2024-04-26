f7cc298e852bde63f6fc097039cd7987b41346d7
phelipeoliveira <phelipewesleydeoliveira@gmail.com>
2024-04-26 16:03:23 -0300

    add border in lsp float

--- init.lua
+++ init.lua
@@ -88,6 +88,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.diagnostic.config {
+  float = { border = 'rounded' },
+}
+
 -- Buffer keymaps
 vim.keymap.set('n', '<C-q>', ':bp|bd #<CR>', { silent = true, desc = 'Close current buffer' })
 vim.keymap.set('n', '<leader>l', ':bnext<CR>', { silent = true, desc = 'Next buffer' })
