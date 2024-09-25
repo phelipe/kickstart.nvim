98034765f91c37913aa4eb87f000bac29c85f7a4
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2024-09-25 18:43:30 -0300

    remove plugins do mini para fazer teste
    talvez seja melhor adicionar separadamente

--- init.lua
+++ init.lua
@@ -728,14 +728,16 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      --  TODO: verify if a realy need this plugin here
+      -- require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- TODO: update surround to use m isntead of s beacuse s is used by other plugin
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
